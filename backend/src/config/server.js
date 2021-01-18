a6ab4ca401e41ce9e5d2f874301780b88e954d44
tessmannaugusto <atessmannyunes@gmail.com>
2021-01-18 11:42:06 -0300

    Finish backend

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,5 +1,6 @@
 const bodyParser = require('body-parser')
 const express = require('express')
+const allowCors = require('./cors')
 
 const port = 3003;
 
@@ -9,6 +10,10 @@ server.use(bodyParser.urlencoded({extended: true}));
 
 server.use(bodyParser.json());
 
+server.use(allowCors);
+
 server.listen(port, function() {
     console.log(`Backend is running on port: ${port}`)
 })
+
+module.exports = server;
