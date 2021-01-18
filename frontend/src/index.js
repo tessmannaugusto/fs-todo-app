79e863af682bfaeed18058551a7ec57e1282571c
tessmannaugusto <atessmannyunes@gmail.com>
2021-01-18 13:31:27 -0300

    create react app

---
+++ frontend/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import App from './main/app'
+
+ReactDom.render(<App></App>, document.getElementById('root'))
---
+++ frontend/src/main/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+
+export default props => {
+    return(
+        <div className='container'>
+        <h1>Teste</h1>
+    </div>)
+}
+   
